6e0762ad573f070ae8344e94afac319dab686db4
tyanaackerman <60484066+tyanaackerman@users.noreply.github.com>
2021-03-11 17:36:52 -0800

    Updates to html, css, and js

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,24 @@ var numRand;
 var max = 3;
 numRand = Math.floor(Math.random() * backgrounds.length);
 
-// Get element
-let pageBackground = document.querySelector("body");
+// // Get element
+// let pageBackground = document.querySelector("body");
 
-// Set class name from stored values
-pageBackground.className = backgrounds[numRand];
+// // Set class name from stored values
+// pageBackground.className = backgrounds[numRand];
 
-// Use Gallery
-lightGallery(document.getElementById("gallery"));
+// // Use Gallery
+// lightGallery(document.getElementById("gallery"));
+
+const gallery = document.querySelector("#gallery");
+const video = document.getElementById("video");
+
+lightGallery(gallery, {
+  selector: "a",
+  mode: "lg-slide-skew-cross",
+  download: false
+});
+
+lightGallery(document.getElementById("captions"));
+
+// lightGallery(video);
